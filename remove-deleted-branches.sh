81285472c8477b988e2f48bf6afa5e40d6a2492c
Ian Buñag <jianbunag@gmail.com>
2022-01-10 10:04:56 +0800

    Initialize shell utils

---
+++ remove-deleted-branches.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Usage:
+# - Alias in preferred shell
+# - Pass mode as first argument
+# - Pass in ignored branch patterns
+#
+# Example:
+#   # Run with no force delete
+#   alias gc="sh /Users/ianbunag/Documents/projects/personal/scripts/remove-deleted-branches.sh normal 'main$' 'master$'"
+#   # Run with force delete
+#   alias gcf="sh /Users/ianbunag/Documents/projects/personal/scripts/remove-deleted-branches.sh force 'main$' 'master$'"
+
+function filter() {
+  local patterns=$*
+
+  if [ $# -gt 0 ]
+    then
+      grep -v $(echo $patterns | sed "s/ /\\\|/g")
+    else
+      cat
+  fi
+}
+
+function remove() {
+  local mode=$1
+  shift
+  local patterns=$*
+  local deleteFlag && [ "$mode" == "force" ] && deleteFlag=D || deleteFlag=d
+
+  local checkedOutPattern='^\*'
+
+  git branch | \
+    filter $checkedOutPattern | \
+    filter $patterns | \
+    cat >/tmp/clean-branches && \
+    vi /tmp/clean-branches && \
+    xargs git branch "-$deleteFlag" </tmp/clean-branches
+}
+
+remove $*
